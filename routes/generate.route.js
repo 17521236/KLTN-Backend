10ef06c0a47b32f20f96bb82752c8f1661899ae7
Tung Nguyen <17521236@gm.uit.edu.vn>
2021-07-11 17:48:21 +0700

    API Generate vehicle, resident

--- routes/generate.route.js
+++ routes/generate.route.js
@@ -7,19 +7,23 @@ const Block = require('../models/block');
 const HELPER = require('../helper');
 const ObjectId = require('mongoose').Types.ObjectId;
 
+const faker = require('faker');
+
 router.post('/vehicle', async (req, res) => {
     try {
         const listResident = await Resident.find({});
         for (let i of listResident) {
-            const newVehicle = {
+            const data = {
                 residentId: i._id,
                 licensePlate: `${Math.round(Math.random() * 100)}C1-${Math.round(Math.random() * 100000)}`,
                 price: Math.round(Math.random() * 100000000),
                 type: "MOTORBIKE"
             }
+            const newVechile = new Vehicle(data);
+            await newVechile.save();
         }
-        res.send(listResident);
-        return;
+        res.send([])
+
     } catch (error) {
         res.status(400).send(HELPER.errorHandler(error, 2000));
         return;
@@ -27,45 +31,42 @@ router.post('/vehicle', async (req, res) => {
 })
 
 router.post('/resident', async (req, res) => {
+    //  /// delete by blockId
+    // try {
+    //     const result = await Resident.deleteMany({ blockId: ObjectId("60cf09e07772c33e808623f2") });
+    //     res.send(result)
+    // } catch (error) {
+    //     res.status(400).send(HELPER.errorHandler(error, 2000));
+    //     return;
+    // }
+    // 60cf09e07772c33e808623f2 - B
+    // 60cf09e07772c33e808623f2 - B
     try {
-        const aptList = await Apartment.find({ blockId: ObjectId("60cf0a2a7772c33e808623f3") });
+        const aptList = await Apartment.find({ blockId: ObjectId("60cf09e07772c33e808623f2") });
         for (let i of aptList) {
             // add account
             let data = {
                 aptId: i._id,
-                blockId: "60cf0a2a7772c33e808623f3",
-                dateOfBirth: 80413000000 + Math.round(Math.random() * 1000000),
+                blockId: "60cf09e07772c33e808623f2",
+                dateOfBirth: 739958000000 + Math.round(Math.random() * 1000000),
                 email: "",
                 identityCard: `245${Math.round(Math.random() * 1000000)}`,
-                name: "Nguyễn Văn Hưng",
+                name: faker.name.findName(),
                 note: "",
-                phoneNumber: "0897556478",
+                phoneNumber: `03${Math.round(Math.random() * 100000000)}`,
                 type: "0"
             };
             data.password = "";
             data.avatarUrl = "";
             data.hasAccount = false;
-            var newResident = new Resident(req.body);
+            var newResident = new Resident(data);
             // end add account
-            let result = await newResident.save();
+            await newResident.save();
         }
-        res.send(aptList);
+        res.send([]);
         return;
     } catch (error) {
         res.status(400).send(HELPER.errorHandler(error, 2000));
-        return;
-    }
-
-
-
-
-    try {
-        //  save
-        let result = await newResident.save();
-        res.send(result);
-        return;
-    } catch (error) {
-        res.status(400).send(HELPER.errorHandler(error, 1000));
         return;
     }
 })
